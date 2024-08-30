2db05837bb64c2b44d7d106978775d806e27b99e
Gustavo Gebhardt <gustavogebhardt@MacBook-Air-de-Gustavo.local>
2024-08-29 22:22:56 -0300

    gitignore

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -4,9 +4,6 @@ use src\controllers\ClienteController;
 use src\controllers\DiretorController;
 use src\controllers\EmprestimoController;
 use src\controllers\FilmeController;
-use src\controllers\HomeController;
-
-$app->get('/', [HomeController::class, 'index']);
 
 $app->post('/filme', [FilmeController::class, 'create']);
 $app->get('/filme', [FilmeController::class, 'findAll']);
